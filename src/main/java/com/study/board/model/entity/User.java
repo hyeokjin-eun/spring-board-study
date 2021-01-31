ba31275bda22bfeb1f80335a4f61d171d90f0f1d
hyeokjin <gurwls2399@gmail.com>
2021-01-31 23:35:19 +0900

    feat : 회원 entity 변경

--- src/main/java/com/study/board/model/entity/User.java
+++ src/main/java/com/study/board/model/entity/User.java
@@ -3,24 +3,35 @@ package com.study.board.model.entity;
 import lombok.*;
 import lombok.experimental.Accessors;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Data
 @Builder
 @Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@ToString
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long seq;
 
+    @Column(columnDefinition = "VARCHAR(100) COMMENT '아이디'", nullable = false)
     private String id;
 
+    @Column(columnDefinition = "VARCHAR(100) COMMENT '비밀번호'", nullable = false)
     private String password;
 
+    @Column(columnDefinition = "VARCHAR(100) COMMENT '이메일'", nullable = false)
     private String email;
 
+    @Column(columnDefinition = "DATETIME COMMENT '생성 일자'", nullable = false)
     private LocalDateTime created;
 
+    @Column(columnDefinition = "DATETIME COMMENT '수정 일자'", nullable = false)
     private LocalDateTime updated;
 }
